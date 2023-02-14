60d572c321b6b75dcc7e548680a9ecadc940bcae
Durvankur Balkawade <durvankurbalkawade@gmail.com>
2023-02-14 15:55:06 +0530

    Digit class used in Number Letter Counts

---
+++ src/Feb14/Digit.java
@@ -0,0 +1,19 @@
+package Feb14;
+
+public class Digit {
+    private String letterValue;
+    private int numValue;
+
+    public Digit(String letterValue, int numValue) {
+        this.letterValue = letterValue;
+        this.numValue = numValue;
+    }
+
+    public String getLetterValue() {
+        return letterValue;
+    }
+
+    public int getNumValue() {
+        return numValue;
+    }
+}
