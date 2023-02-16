3e83aaf4ab95036a9a5b5ef1cc30b2980e74edf3
Durvankur Balkawade <durvankurbalkawade@gmail.com>
2023-02-16 17:44:37 +0530

    added Lychrel Numbers Code

---
+++ src/Feb16/LychrelNumbers.java
@@ -0,0 +1,62 @@
+/*
+Q.55 Lychrel Numbers
+"If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+Not all numbers produce palindromes so quickly. For example,
+349 + 943 = 1292,
+1292 + 2921 = 4213
+4213 + 3124 = 7337
+That is, 349 took three iterations to arrive at a palindrome.
+Although no one has proved it yet, it is thought that some numbers, like 196, never produce a palindrome. A number that never forms a palindrome through the reverse and add process is called a Lychrel number. Due to the theoretical nature of these numbers, and for the purpose of this problem, we shall assume that a number is Lychrel until proven otherwise. In addition you are given that for every number below ten-thousand, it will either (i) become a palindrome in less than fifty iterations, or, (ii) no one, with all the computing power that exists, has managed so far to map it to a palindrome. In fact, 10677 is the first number to be shown to require over fifty iterations before producing a palindrome: 4668731596684224866951378664 (53 iterations, 28-digits).
+Surprisingly, there are palindromic numbers that are themselves Lychrel numbers; the first example is 4994.
+How many Lychrel numbers are there below ten-thousand?
+NOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretical nature of Lychrel numbers."
+ */
+package Feb16;
+
+public class LychrelNumbers {
+
+
+    public static long reverse(long number){
+        long reverse = 0;
+
+        while (number>0){
+            long rem = number%10;
+            reverse = (reverse*10) + rem;
+            number/=10;
+        }
+        return reverse;
+    }
+
+    public static boolean isPallindrome(long number){
+        return number == reverse(number);
+    }
+
+    public static boolean isLychrel(long number){
+        for (int i=0;i<49;i++)
+        {
+            number = number+reverse(number);
+            if(isPallindrome(number))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static int countLychrel(int limit)
+    {
+        int count = 0;
+        for (int i=0;i<limit;i++)
+        {
+            if (isLychrel(i))
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println(countLychrel(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("Execution Time: "+(end-start)/1000F+" seconds");
+    }
+}
